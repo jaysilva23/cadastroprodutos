69039fa44f9a2f87a52a0d951bcd936042cc57bd
Jaiane Silva <jaiane9638@gmail.com>
2022-06-21 22:06:19 -0300

    first

---
+++ Produtos/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produtos.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Fabricante { get; set; }
+        public double Valor { get; set; }
+        public int Codigo { get; set; }
+    }
+}
---
+++ Produtos/Program.cs
@@ -0,0 +1,40 @@
+﻿using Produtos.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Produtos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<Produto>();
+            Console.WriteLine("Quantos produtos você quer adicionar na lista? ");
+            var qtd = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < qtd; i++)
+            {
+                lista.Add(AskProduto());
+            }
+
+            Console.ReadLine();
+        }
+
+        private static Produto AskProduto()
+        {
+            var prod = new Produto();
+            Console.WriteLine("Insira aqui o nome do produto: ");
+            prod.Nome = Console.ReadLine();
+            Console.WriteLine("Insira aqui a descrição do produto: ");
+            prod.Descricao = Console.ReadLine();
+            Console.WriteLine("Insira aqui o nome do fabricante: ");
+            prod.Fabricante = Console.ReadLine();
+            Console.WriteLine("Insira aqui o valor do produto: ");
+            prod.Valor = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira aqui o código do produto: ");
+            prod.Codigo = int.Parse(Console.ReadLine());
+            
+            return prod;
+        }
+    }
+}
